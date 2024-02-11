7fb254f84f679032726a3209e8d4523ab58ccf15
sreejith8129 <139565025+sreejith8129@users.noreply.github.com>
2024-02-11 21:45:02 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Box, Typography, Button, Stack, } from "@mui/material";
+import React, {useState} from "react";
 
-function App() {
+export default function Counter() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container sx={containerStyle}>
+        <Box sx={Boxstyle}>
+          <Typography align="center" variant="h1" gutterBottom>
+            {count}
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            
+          <Button variant="contained" color="success"  onClick={() => setCount(count + 1)}>Increment</Button>
+          <Button variant="contained" color="secondary" onClick={()=> setCount(0)} disabled={count === 0}>Reset</Button>
+          <Button variant="contained" color="error"  onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</Button>
+          </Stack>
+        </Box>
+      </Container>
+    </>
   );
 }
 
-export default App;
+const Boxstyle = {
+  backgroundColor: "lightblue",
+  borderRadius: "5px",
+  width: "50%",
+  padding:"80px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px "
+
+}
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "700px",
+}
